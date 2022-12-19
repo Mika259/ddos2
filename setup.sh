fb54bdccc906a195d151260fe3e5401838e865b2
M1K259 <87774553+Mika259@users.noreply.github.com>
2022-12-19 12:18:05 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+!#bin/bash
+
+clear
+sleep 1
+echo "setup installing figlet"
+sleep 1
+apt install figlet -y
+sleep 1
+echo "setup installing toilet"
+sleep 1
+apt install toilet
+sleep 1
+echo "setup success"
+echo ""
+echo "installed figlet,toilet"
+sleep 2
+clear
